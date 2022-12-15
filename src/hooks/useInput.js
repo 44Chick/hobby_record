44b817cae4f55f131284686788d86bbedaec090d
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-15 18:10:41 +0900

    Add custom hook (temporary)

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react"
 
-const useInput = () => {
+/* const useInput = () => {
   const [value, setValue] = useState("")
   const handler = (e) => {
     setValue(e.target.value)
+  }
+  return [value, handler]
+} */
+
+const useInput = (e) => {
+  const [content, setContent] = useState("")
+  const { name, value } = e.target
+  const handler = (e) => {
+    setContent({ ...content, [name]: value })
   }
   return [value, handler]
 }
